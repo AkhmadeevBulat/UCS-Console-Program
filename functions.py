d445d91c972ab66a4b2798507e0e401399d8f92d
Akhmadeev Bulat Nailevich <bulat-axxxmadeev@mail.ru>
2024-03-26 15:36:29 +0300

    .

--- functions.py
+++ functions.py
@@ -2,12 +2,14 @@ from sys import platform
 from os import system
 from time import sleep, localtime, strftime
 import functools
+from pathlib import Path
+from dotenv import dotenv_values
 
 
-def clear_terminal():  # Чистка терминала
-    if platform == 'darwin' or platform == 'linux' or platform == 'linux2':
+def clear_terminal(pltf: str):  # Чистка терминала
+    if pltf == 'darwin' or pltf == 'linux' or pltf == 'linux2':
         system('clear')
-    elif platform == 'win32':
+    elif pltf == 'win32':
         system('cls')
 
 
@@ -22,4 +24,41 @@ def handle_keyboard_interrupt(func):
             return func(*args, **kwargs)
         except KeyboardInterrupt:
             print()
+
     return wrapper
+
+
+def find_file(pltf: str):
+    if pltf == 'darwin':
+        home_dir = Path.home()  # Получаем путь к домашней директории пользователя
+        file_path = home_dir / '.env-ucs'  # Формируем путь к файлу ~/.env-ucs.py
+        return file_path
+
+
+def create_env_file(pltf: str):
+    if pltf == 'darwin':
+        env_path = Path.home() / '.env-ucs'
+        file_path = env_path / '.env-ucs'  # Формируем путь к файлу ~/.env-ucs.py
+        with open(env_path, 'w') as file:
+            file.write("")
+        return file_path
+
+
+def check_env_variables():
+    env_variables = dotenv_values('~/.env-ucs')
+    required_variables = ['login_network',
+                          'password_network',
+                          'login_ad',
+                          'password_ad',
+                          'login_email',
+                          'password_email']
+    missing_variables = []
+
+    for variable in required_variables:
+        if variable not in env_variables:
+            missing_variables.append(variable)
+
+    if len(missing_variables) == 0:
+        print("Все переменные присутствуют в файле .env-ucs")
+    else:
+        print("Следующие переменные отсутствуют в файле .env-ucs:", missing_variables)
--- main.py
+++ main.py
@@ -1,34 +1,83 @@
+import os
 import paramiko
 import json
 from time import sleep, localtime, strftime
 from os import system
-from sys import platform
 import re
 from string import Template
 from colorama import init, Fore, Back, Style
 from getpass import getpass
 from simple_term_menu import TerminalMenu
+from sys import platform
 
-from functions import handle_keyboard_interrupt, clear_terminal
+from functions import handle_keyboard_interrupt, clear_terminal, find_file, create_env_file
 
 
 @handle_keyboard_interrupt
-def main():
+def main(pltf: str):
     while True:  # Диалог с администратором
-        clear_terminal()
-        menu = ["Поиск устройства по ip/mac адресу", "Настройка зон", "", "Настройка программы"]
+        clear_terminal(pltf)
+        menu = ["Поиск устройства по ip/mac адресу",
+                "Настройка зон",
+                "Настройка интерфейса коммутатора",
+                "",
+                "Настройка программы",
+                "",
+                "Выход"]
         terminal_menu = TerminalMenu(menu, title="Выберете действие:", skip_empty_entries=True)
         menu_entry_index = terminal_menu.show()
-        print(f"Ты выбрал: {menu[menu_entry_index]}!")
+        # print(f"Ты выбрал: {menu[menu_entry_index]}!")  # Получить имя выбранного
+        # print(f"Ты выбрал: {terminal_menu.chosen_menu_index}!")  # Получить индекс выбранного
 
-        x = input()
+        match menu[menu_entry_index]:
+            case "Поиск устройства по ip/mac адресу":
+                pass
+            case "Настройка зон":
+                pass
+            case "Настройка программы":
+                while True:
+                    menu_settings = ["Очистить log файл",
+                                     "Изменить логин и пароль для подключения 'Сеть'",
+                                     "Изменить логин и пароль для подключения 'Active Directory'",
+                                     "Изменить логин и пароль для подключения 'Exchange'",
+                                     "",
+                                     "Назад"]
+                    terminal_menu = TerminalMenu(menu_settings, title="Меню настроек:", skip_empty_entries=True)
+                    menu_entry_index = terminal_menu.show()
+
+                    match menu_settings[menu_entry_index]:
+                        case "Очистить log файл":
+                            pass
+                        case "Изменить логин и пароль для подключения 'Сеть'":
+                            pass
+                        case "Изменить логин и пароль для подключения 'Active Directory'":
+                            pass
+                        case "Изменить логин и пароль для подключения 'Exchange'":
+                            pass
+                        case "Назад":
+                            break
+            case "Выход":
+                break
 
 
-def before_main():  # Проверка переменных и подключений к важным ресурсам
-    main()
-    pass
+def before_main(pltf: str):  # Проверка файлов, переменных и подключений к важным ресурсам
+
+    # Проверка на файл с переменными окружения
+    file_path = find_file(pltf)
+    if file_path.exists():
+        print("Файл ~/.env-ucs найден!")
+    else:
+        print("Файл ~/.env-ucs не найден!")
+        create_env_file(pltf)
+        print("Файл ~/.env-ucs создан!")
+
+    # Проверка присутствие правильных переменных в файле
+
+
+    sleep(2)
+    main(pltf=pltf)
 
 
 if __name__ == '__main__':
     init(autoreset=True)
-    before_main()
+    before_main(pltf=platform)
